9765ac62d0fac59757b31495e1f11b0e2ab65b5b
Danilo Rossi <danilorossi83@gmail.com>
2025-03-20 18:40:32 +0100

    Fix script path and ignores

--- chatty.bat
+++ chatty.bat
@@ -3,10 +3,8 @@
 set "SCRIPT_DIR=%~dp0"
 echo Script directory: %SCRIPT_DIR%
 
-cd %SCRIPT_DIR%\..
 call Scripts\activate
 
-cd bot
 if exist chatty.env.bat (
     call chatty.env.bat
 )
--- chatty.sh
+++ chatty.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
-cd /redberry/chatty.v2
-
 source bin/activate
 
-cd bot
 if [ -f "chatty.env" ]; then  source "chatty.env"; fi
 python bot.py
